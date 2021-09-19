47d8a50539ae5d6c71641f574d0b197b0063b28b
mijael03 <mijael.cama@tecsup.edu.pe>
2021-09-18 21:57:13 -0500

    Ejercicios Propuestos

--- lab02/encuesta/urls.py
+++ lab02/encuesta/urls.py
@@ -7,4 +7,8 @@ app_name='encuesta'
 urlpatterns = [
     path('',views.index,name='index'),
     path('enviar',views.enviar,name='enviar'),
+    path('numbers',views.numbers, name='numbers'),
+    path('calc',views.calcular,name='calc'),
+    path('cilindro',views.cilindro,name='cilindro'),
+    path('result',views.result,name='result')
 ]
--- lab02/encuesta/views.py
+++ lab02/encuesta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import math
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,37 @@ def enviar(request):
         'website': request.POST['sitioweb'],
     }
     return render(request, 'encuesta/respuesta.html',context)
+
+def numbers(request):
+    return render(request,'encuesta/numbers.html')
+
+def calcular(request):
+    n1=request.POST['number1']
+    n2=request.POST['number2']
+    operation=request.POST['operation']
+    if(operation=="resta"):
+        result=int(n1)-int(n2)
+    elif(operation=="suma"):
+        result=int(n1)+int(n2)
+    elif(operation=="multiplicacion"):
+        result=int(n1)*int(n2)
+    context = {
+        'number1': n1,
+        'number2': n2,
+        'operation': operation,
+        'result': result,
+    }
+    return render(request, 'encuesta/result.html',context)
+
+def cilindro(request):
+    return render(request,'encuesta/cilindro.html')
+
+def result(request):
+    pi=math.pi
+    h=request.POST['altura']
+    r=float(request.POST['diametro'])/2
+    result=pi*float(h)*(r**2)
+    context = {
+        'result': result,
+    }
+    return render(request,'encuesta/volumen.html',context)
