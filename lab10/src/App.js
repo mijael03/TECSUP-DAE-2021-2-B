85928746fbb6ffa5d721c84492154adea2b5beac
mijael03 <mijael.cama@tecsup.edu.pe>
2021-10-31 22:05:00 -0500

    React

---
+++ lab10/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Title from './components/Title'
+
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: []
+    }
+  }
+  componentWillMount() {
+    fetch('http://localhost:8000/api/productos')
+      .then((response) => {
+        return response.json()
+      })
+      .then((prod) => {
+        this.setState({ productos: prod })
+      })    
+  } 
+  render() {
+    return (
+        <div>
+          <Layout>
+            <Title/>
+            <Productos
+              productos={this.state.productos}
+            />
+          </Layout>
+        </div>
+      )
+    }
+  
+
+}
+
+export default App;
